601ce1b1f989359d3ce4057891b72178e541ad2d
Евгения <j.nikif15@gmail.com>
2022-08-06 17:33:47 +0300

    Добавлено решение задачи 2

---
+++ Zadaha2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ниписать программу, которая на вход принимает 2 числа и выдает какое число больше, а какое меньше
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB){Console.WriteLine("Первое число больше второго и равно: " + numberA);}
+if (numberA < numberB){Console.WriteLine("Второе число больше первого и равно: " + numberB);}
