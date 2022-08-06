ca997198bb87e65dd4f7e0166ec757ed37927b30
Евгения <j.nikif15@gmail.com>
2022-08-06 15:21:28 +0300

    Добавлено решение задачи 8

---
+++ Zadaha8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на входе принимает число (N), а на выходе показывает все четные числа от 1 до N
+Console.WriteLine("Введите число большее 1 ");
+int N = int.Parse(Console.ReadLine());
+
+if (N < 1){Console.WriteLine("Вы ввели число меньше 1");}
+
+int index = 1;
+
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine("Четное число: " + index);
+    }
+    index ++;
+}
