a7c4a6364283c02940f5998076a9e5dd903cb12a
Евгения <j.nikif15@gmail.com>
2022-08-10 21:56:55 +0300

    Добавлено решение задачи 13

---
+++ Zadaha13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.WriteLine("Напишите целое число: ");
+int num = int.Parse(Console.ReadLine());
+string str = num.ToString();
+if (str.Length < 3)
+{
+    Console.WriteLine("В заданном числе третьей цифры нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра заданного числа: " + str[2]);
+}
