5857cf1d9a1423dd5d7b0356f954df0a98f7a49f
Евгения <j.nikif15@gmail.com>
2022-08-14 19:25:15 +0300

    Добавлено изменение в решение задачи 19

--- Zadaha19/Program.cs
+++ Zadaha19/Program.cs
@@ -2,7 +2,11 @@
 Console.WriteLine("Введите пятизначное число: ");
 string leg = Console.ReadLine();
 
-if (leg[0] == leg[leg.Length - 1] && leg[1] == leg[leg.Length - 2])
+if (leg.Length < 5 || leg.Length > 5)
+{
+    Console.WriteLine("Вы ввели не пятизначное число!");
+}
+else if (leg[0] == leg[leg.Length - 1] && leg[1] == leg[leg.Length - 2])
 {
     Console.WriteLine("Число является палиндромом!");
 }
