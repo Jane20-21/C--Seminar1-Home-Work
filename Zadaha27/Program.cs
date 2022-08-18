b74383d5ca4ac50888edfc5907da8292b36d93b9
Евгения <j.nikif15@gmail.com>
2022-08-18 20:44:17 +0300

    Добавлено решение 27 задачи

---
+++ Zadaha27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает на вход число и выдает сумму цифр в числе
+
+int str(string num)
+{
+    int sum = 0;
+    for(int i = 0; i < num.Length; i++)
+    {
+        sum += int.Parse(num[i].ToString());
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+Console.WriteLine("Сумма цифр в числе: " + str(number));
