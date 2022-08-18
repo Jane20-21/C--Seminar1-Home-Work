97a34e0a77324d07e212f298574939ec9843edef
Евгения <j.nikif15@gmail.com>
2022-08-18 20:44:44 +0300

    Добавлено решение 29 задачи

---
+++ Zadaha29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+
+void NewArray(int[] dif)
+{
+    for(int i = 0; i < dif.Length; i++)
+    {
+        dif[i] = new Random().Next(1, 10);
+        Console.Write($"{dif[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[8];
+NewArray(array);
