002ca72499065e71c8190a9c74d61c26c2bfed36
Евгения <j.nikif15@gmail.com>
2022-08-18 20:43:45 +0300

    Добавлено решение 25 задачи

---
+++ Zadaha25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл который принимает на вход два числа (А и В) и возводит число А в натуральную степень В
+
+int Fit(int a, int b)
+{
+    return Convert.ToInt32(Math.Pow(a, b));
+}
+
+Console.Write("Введите целое число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Число А в степени В равно: " + Fit(A, B));
