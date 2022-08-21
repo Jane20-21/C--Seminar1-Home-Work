4f7a47ebce673530fce4a91942ed69b0c408b9d7
Евгения <j.nikif15@gmail.com>
2022-08-21 17:03:38 +0300

    Добавлено решение задачи 34

---
+++ Zadaha34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. Напишите программу, которая покажет колличество четных чисел в массиве
+
+int[] generateArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    Console.WriteLine(string.Join(",", array));
+    return array;
+}
+
+void findEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0){count++;}
+    }
+    Console.WriteLine("Колличество четных чисел в массиве: " + count);
+}
+
+Console.Write("Задайте длину массива: ");
+int length = int.Parse(Console.ReadLine());
+int[] array = new int[length];
+findEven(generateArray(array));
