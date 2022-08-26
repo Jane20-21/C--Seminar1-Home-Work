76e4bb013c95ec0172e05c6d7b098215d59bdef3
Евгения <j.nikif15@gmail.com>
2022-08-26 18:49:11 +0300

    Добавлено решение 41 задачи

---
+++ Zadaha41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте сколько чисел больше 0 ввел пользователь 
+Console.Write("Введите произвольное колличество чисел через запятую: ");
+string str = Console.ReadLine();
+string[] strArray = str.Split(',');
+int[] array = new int[strArray.Length];
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = int.Parse(strArray[i]);
+}
+
+int countNumber(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Колличество положительных чисел равно: " + countNumber(array));
