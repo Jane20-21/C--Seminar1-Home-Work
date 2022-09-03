6201ee8cb50ca4e435e8fadc8a028463ec9ab4be
Евгения <j.nikif15@gmail.com>
2022-09-03 18:32:25 +0300

    Добавлено решение задачи 54

---
+++ Zadaha54/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = 0;
+            if (array[i, temp] < array[i, j])
+            {
+                int number = array[i, temp];
+                array[i, temp] = array[i, j];
+                array[i, j] = number;
+                temp++;
+            } 
+        }
+    }
+    return array;
+}
+
+int[,] array = new int[3,4];
+PrintArray(FillArray(array));
+Console.WriteLine();
+PrintArray(ChangeArray(array));
+
