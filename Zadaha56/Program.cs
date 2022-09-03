32f97135949a956ff328a1edd4876a66f8993f93
Евгения <j.nikif15@gmail.com>
2022-09-03 18:32:50 +0300

    Добавлено решение задачи 56

---
+++ Zadaha56/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int[,] FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindLine(int[,] array)
+{
+    int sum = 0;
+    int minSum = 0;
+    int numberLine = 1;
+    for (int i = 0; i < 1; i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            minSum += array[i, j];
+        }
+    }
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < minSum)
+        {
+            minSum = sum;
+            numberLine = i + 1;
+        }
+    }
+    Console.WriteLine($"Наименьшая сумма элементов находится в строке: {numberLine} Cумма элементов которой равна: {minSum}");
+}
+
+int[,] array = new int[3, 4];
+PrintArray(FillArray(array));
+FindLine(array);
