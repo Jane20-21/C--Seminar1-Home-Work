2848b2bbf8acc3fb5fe17636ef4ffb8137ba483d
Евгения <j.nikif15@gmail.com>
2022-09-05 21:55:26 +0300

    Добавлено решение задачи 64

---
+++ Zadaha64/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+string Numbers(int m, int n)
+{
+    if (m <= n) return $"{m}," + Numbers(m + 1, n);
+    else return String.Empty;
+}
+Console.WriteLine(Numbers(1, 20));
